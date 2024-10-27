f78a3a60e442be76abb3b065396ad3db49c02d5b
MEDOYED <mmax26072005@gmail.com>
2024-10-27 09:52:17 +0100

    header

--- src/components/widgets/header/header.jsx
+++ src/components/widgets/header/header.jsx
@@ -1,30 +1,35 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import logo from "../../../assets/img/header-logo.svg";
+
 import "./header.scss";
 
 class Header extends Component {
   render() {
     return (
-      <nav>
-        <ul>
-          <li key="home">
-            <Link className="header-link" to={"/"}>
-              Coffee House
-            </Link>
-          </li>
-          <li key="our">
-            <Link className="header-link" to={"/our"}>
-              Our coffee
-            </Link>
-          </li>
-          <li key="for">
-            <Link className="header-link" to={"/for"}>
-              For your pleasure
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="container">
+        <nav className="header">
+          <ul className="header-list">
+            <li className="header-item" key="home">
+              <Link className="header-link" to={"/"}>
+                <img className="header-img" src={logo} alt="Header logo" />
+                <p>Coffee House</p>
+              </Link>
+            </li>
+            <li className="header-item" key="our">
+              <Link className="header-link" to={"/our"}>
+                <p>Our coffee</p>
+              </Link>
+            </li>
+            <li className="header-item" key="for">
+              <Link className="header-link" to={"/for"}>
+                <p>For your pleasure</p>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     );
   }
 }
