8a7de400eb48bbcf50734c708b3a0657f791651a
MEDOYED <mmax26072005@gmail.com>
2024-10-30 18:49:55 +0100

    about section on the main page

--- src/components/pages/coffee-house/coffee-house.jsx
+++ src/components/pages/coffee-house/coffee-house.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import CoffeeHouseHeader from "../../widgets/cofee-house-header/coffee-house-header";
+import CoffeeHouseAbout from "../../widgets/cofee-house-about/coffee-house-about";
 
 import "./coffee-house.scss";
 
@@ -9,6 +10,7 @@ class CoffeeHouse extends Component {
     return (
       <>
         <CoffeeHouseHeader></CoffeeHouseHeader>
+        <CoffeeHouseAbout />
       </>
     );
   }
