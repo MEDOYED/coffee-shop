248c724cc76e09a3ac4da30fdbabaa65b450f27f
MEDOYED <mmax26072005@gmail.com>
2024-11-06 15:53:29 +0100

    second page | header on our coffee

--- src/components/pages/our-coffee/our-coffee.jsx
+++ src/components/pages/our-coffee/our-coffee.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 
+import OurCoffeHeader from "../../widgets/our-coffee-header/our-coffee-header";
+
 import "./our-coffee.scss";
 
 class OurCoffee extends Component {
   render() {
     return (
-      <>
-        <h1>Our Coffee</h1>
-      </>
+      <div className="container">
+        <OurCoffeHeader />
+      </div>
     );
   }
 }
---
+++ src/components/widgets/our-coffee-header/our-coffee-header.jsx
@@ -0,0 +1,22 @@
+import { Component } from "react";
+
+import headerImg from "../../../assets/img/our-coffee/our-coffee-header-img.jpeg";
+
+import "./our-coffee-header.scss";
+
+class OurCoffeHeader extends Component {
+  render() {
+    return (
+      <div className="our-coffee-header">
+        <img
+          className="our-coffee-header__img"
+          src={headerImg}
+          alt="header on our coffee page"
+        />
+        <h2 className="our-coffee-header__title">Our Coffee</h2>
+      </div>
+    );
+  }
+}
+
+export default OurCoffeHeader;
