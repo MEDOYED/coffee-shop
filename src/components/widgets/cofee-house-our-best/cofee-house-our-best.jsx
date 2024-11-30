d8d503defda46c8ca6ce2dd6c1ce06421bdcb970
MEDOYED <mmax26072005@gmail.com>
2024-11-30 14:04:11 +0100

    fixed bug on cards on Coffee House page | (in link on page and country on product page

--- src/components/widgets/cofee-house-our-best/cofee-house-our-best.jsx
+++ src/components/widgets/cofee-house-our-best/cofee-house-our-best.jsx
@@ -12,25 +12,33 @@ import "./coffee-house-our-best.scss";
 class CoffeeHouseOurBest extends Component {
   data = [
     {
+      id: 1,
       src: solimoImg,
       description: "Solimo Coffee Beans 2 kg",
       price: "10.73$",
+      country: "Brazil",
     },
     {
+      id: 2,
       src: prestoImg,
       description: "Presto Coffee Beans 1 kg",
       price: "15.99$",
+      country: "Kenya",
     },
     {
+      id: 3,
       src: oromistoImg,
       description: "AROMISTICO Coffee 1 kg",
       price: "6.99$",
+      country: "Columbia",
     },
   ];
 
   renderCards() {
     return this.data.map((item, index) => (
       <ProductCard
+        id={item.id}
+        country={item.country}
         key={index}
         src={item.src}
         description={item.description}
