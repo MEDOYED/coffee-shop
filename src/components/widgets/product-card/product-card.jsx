92a98e6742fa8c8e0cac4dfcf45c9153a97855b4
MEDOYED <mmax26072005@gmail.com>
2024-11-30 13:45:50 +0100

    fixed ui bug on cards on Coffee House and Our Coffee pages

--- src/components/widgets/product-card/product-card.jsx
+++ src/components/widgets/product-card/product-card.jsx
@@ -14,14 +14,14 @@ class ProductCard extends Component {
     return (
       <li className={`home-card  ${shadowClass}`}>
         <Link
-          className="home-card"
+          className="home-cards"
           to={`/product/${id}`}
           state={{ id, src, description, country, price }}
         >
-          <img className="home-card__img" src={src} alt="Coffee" />
-          <p className="home-card__description">{description}</p>
+          <img className="home-cards__img" src={src} alt="Coffee" />
+          <p className="home-cards__description">{description}</p>
           {countryParagraph}
-          <p className="home-card__price">{price}</p>
+          <p className="home-cards__price">{price}</p>
         </Link>
       </li>
     );
