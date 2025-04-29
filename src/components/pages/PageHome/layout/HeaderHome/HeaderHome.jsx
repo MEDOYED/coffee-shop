fa515243780132c002505710c6134801940cd46d
MEDOYED <mmax26072005@gmail.com>
2025-04-29 21:11:58 +0200

    refactor(HeaderHome): improve BEM naming and styles

--- src/components/pages/PageHome/layout/HeaderHome/HeaderHome.jsx
+++ src/components/pages/PageHome/layout/HeaderHome/HeaderHome.jsx
@@ -6,19 +6,21 @@ import "./HeaderHome.scss";
 
 const HeaderHome = () => {
   return (
-    <header className="home-header">
-      <div className="home-header__content">
-        <h1 className="home-header__content-title">
+    <header className="header-home">
+      <div className="header-home__content">
+        <h1 className="header-home__content-title">
           Everything You Love About Coffee
         </h1>
 
         <WhiteBeen />
 
-        <h2 className="home-header-subtitle">
+        <h2 className="header-home__content-subtitle">
           We makes every day full of energy and taste.
         </h2>
-        <h2 className="home-header-subtitle">Want to try our beans?</h2>
-        <button className="home-header-btn">
+        <h2 className="header-home__content-subtitle">
+          Want to try our beans?
+        </h2>
+        <button className="header-home__content-btn">
           <Link to={"/our"}>More</Link>
         </button>
       </div>
