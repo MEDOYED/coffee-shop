3732d4d5488ef34f4304b2d8c567903dbf23dfe8
MEDOYED <mmax26072005@gmail.com>
2025-04-30 12:32:00 +0200

    feat: create dataProductCard.js

---
+++ src/shared/data/dataProductCard.js
@@ -0,0 +1,50 @@
+import solimo from "../../shared/assets/img/home/home-solimo.png";
+import presto from "../../shared/assets/img/home/home-presto.jpeg";
+import aromisto from "../../shared/assets/img/home/home-aromisto.jpeg";
+
+const dataProductCard = [
+  {
+    id: 1,
+    src: solimo,
+    description: "Solimo Coffee Beans 1 kg",
+    country: "Brazil",
+    price: "6.99$",
+  },
+  {
+    id: 2,
+    src: presto,
+    description: "Presto Coffee Beans 2 kg",
+    country: "Kenya",
+    price: "9.99$",
+  },
+  {
+    id: 3,
+    src: aromisto,
+    description: "AROMISTICO Coffee 1 kg",
+    country: "Columbia",
+    price: "6.99$",
+  },
+  {
+    id: 4,
+    src: aromisto,
+    description: "AROMISTICO Coffee 1 kg",
+    country: "Brazil",
+    price: "11.99$",
+  },
+  {
+    id: 5,
+    src: solimo,
+    description: "Solimo Coffee Beans 1 kg",
+    country: "Kenya",
+    price: "10.99$",
+  },
+  {
+    id: 6,
+    src: presto,
+    description: "Presto Coffee Beans 2 kg",
+    country: "Columbia",
+    price: "13.99$",
+  },
+];
+
+export default dataProductCard;
