2230c72cc0902e13536f4202a95f59295927cb30
MEDOYED <mmax26072005@gmail.com>
2025-08-14 14:05:44 +0200

    fix(PageProductInfo): fix deployment

--- src/pages/PageProductInfo/PageProductInfo.jsx
+++ src/pages/PageProductInfo/PageProductInfo.jsx
@@ -1,16 +1,16 @@
-import OurCoffeHeader from "../../widgets/our-coffee-header/our-coffee-header";
+import SectionHero from "../../widgets/sections/SectionHero/SectionHero";
 import SectionAboutProduct from "../../widgets/sections/SectionAboutProduct/SectionAboutProduct";
 import BlackBeen from "../../shared/ui/black-been/black-been";
 import Header from "../../widgets/layout/Header/Header";
+
+import headerImg from "../../shared/assets/img/our-coffee/our-coffee-header-img.jpeg";
 
 import "./PageProductInfo.scss";
 
 const PageProductInfo = () => {
   return (
     <section>
-      <header className="our-coffee-header">
-        <OurCoffeHeader />
-      </header>
+      <SectionHero imageSrc={headerImg} title={"Product Info"} />
       <section className="product-about-it">
         <SectionAboutProduct />
       </section>
